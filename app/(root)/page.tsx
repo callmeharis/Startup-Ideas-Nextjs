d52ef94f3b94249aae5de273d5d096b22076fe0b
callmeharis <theharis99@gmail.com>
2024-11-12 19:58:54 +0500

    homepage structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,52 @@
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Haris" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://cdn.pixabay.com/photo/2024/03/20/03/06/ai-generated-8644499_1280.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup <br />
+          Conenct With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote on Pitches, and Get Notices in Virtual
+          Competetions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
+  );
+}
